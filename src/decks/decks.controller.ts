86eba8646514dcb78ef29406e8fc38679cd79b6b
Arthur Wang <75041710+aardvark-warrior@users.noreply.github.com>
2023-11-18 17:22:56 -0800

    Implement PATCH endpoint for updating posts

--- src/decks/decks.controller.ts
+++ src/decks/decks.controller.ts
@@ -1,15 +1,20 @@
-import { Get, Param, Post, Body, Controller, NotFoundException } from '@nestjs/common';
+import { Patch, Get, Param, Post, Body, Controller, NotFoundException } from '@nestjs/common';
 import { DecksService } from './decks.service';
 import { CreateDeckDto } from './create-deck.dto';
 import { DeckResponseDto } from './deck-response.dto';
+import { UserId } from 'src/decorators/user-id.decorator';
+import { UpdateDeckDto } from './update-deck.dto';
 
 @Controller('decks')
 export class DecksController {
     constructor(private readonly decksService: DecksService) {}
 
     @Post()
-    async create(@Body() createDeckDto: CreateDeckDto): Promise<DeckResponseDto> {
-        const userId = 1; // TODO: get userId from JWT token
+    async create(
+        @Body() createDeckDto: CreateDeckDto,
+        @UserId() userId: number,
+    ): Promise<DeckResponseDto> {
+        // const userId = 1; // TODO: get userId from JWT token
         const deck = await this.decksService.create(createDeckDto, userId);
         delete deck.userId;
         return deck;
@@ -24,6 +29,20 @@ export class DecksController {
         delete deck.userId;
         return deck;
     }
+
+    @Patch(':id')
+    async update(
+        @Param('id') id: string,
+        @Body() updateDeckDto: UpdateDeckDto,
+    ): Promise<DeckResponseDto> {
+        const deck = await this.decksService.update(id, updateDeckDto);
+        if (!deck) {
+            throw new NotFoundException(`Deck with ID ${id} not found`);
+        }
+        delete deck.userId;
+        return deck;
+    }
+
 
 
     // We will add handlers for CRUD endpoints here
--- src/decks/decks.service.ts
+++ src/decks/decks.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Deck } from './deck.entity';
 import { CreateDeckDto } from './create-deck.dto';
+import { UpdateDeckDto } from './update-deck.dto';
 
 @Injectable()
 export class DecksService {
@@ -11,6 +12,7 @@ export class DecksService {
         private deckRepository: Repository<Deck>,
     ) {}
 
+    // Create a new Deck
     async create(createDeckDto: CreateDeckDto, userId: number): Promise<Deck> {
         const deck = await this.deckRepository.create({
             ...createDeckDto,
@@ -19,9 +21,20 @@ export class DecksService {
         return this.deckRepository.save(deck);
     }
 
+    // Find and return a Deck
     async findOne(id: string): Promise<Deck | null> {
         return this.deckRepository.findOneBy({ id });
     }
+
+    // Update a Deck
+    async update(id: string, updateDeckDto: UpdateDeckDto): Promise<Deck | null> {
+        const deck = await this.deckRepository.preload({ id, ...updateDeckDto });
+        if (!deck) {
+            return null;
+        }
+        return this.deckRepository.save(deck);
+    }
+      
       
       
 
